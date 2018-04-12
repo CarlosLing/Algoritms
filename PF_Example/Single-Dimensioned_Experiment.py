f70fb6121522ba5f61a844708c5ff38e4abe0253
CarlosLing <37625533+CarlosLing@users.noreply.github.com>
2018-04-12 18:45:40 -0400

    Particle filter preliminary implementation
    Further development must be implemented concerning importance function and weight computation

--- PF_Example/Single-Dimensioned_Experiment.py
+++ PF_Example/Single-Dimensioned_Experiment.py
@@ -3,25 +3,26 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.stats import norm
 
-def f(x, k, w):
-    return x/2 + 25*x / (1 + x**2) + 8 * np.cos(1.2 * (k-1)) + w
+
+def f(a, b, c):  # Hidden states function
+    return a/2 + 25*a / (1 + a**2) + 8 * np.cos(1.2 * b) + c
 
 
-def h(x, v):
-
-    return x**2 / 20 + v
+def h(a, b):  # Measured states function
+    return a**2 / 20 + b
 
 
 # Divides the sample according to the weights
 def divide(X, W):
-    Nef = np.round(1/sum(W**2))
+    Nef = int(np.rint(1/sum(W**2)))
     X_s = np.argsort(X)
     index_l = X_s[:Nef]
     index_h = X_s[Nef:]
+    print(Nef)
     return index_l, index_h
 
 
-L = 5000  # Longitude of the series
+L = 300  # Longitude of the series
 # noise vectors:
 sigma_w = np.sqrt(2)
 sigma_v = 1
@@ -41,20 +42,21 @@ for i in range(L):
     if i == 0:
         X_true[i] = x_0
     else:
-        X_true[i] = f(X_true[i], i, w[i])
+        X_true[i] = f(X_true[i-1], i-1, w[i])
     Y_true[i] = h(X_true[i], 0)
 
 Y_measured = Y_true + v
 
-N = 100  # Number of points from the particle filter
+N = 200  # Number of points from the particle filter
 
 # Probability and importance functions to weight calculations
-q = lambda x, y: np.exp(- (y-h(x, 0))**2 / (2 * sigma_v**2))
+
+
+# q = lambda x, y: np.exp(-(y-h(x, 0))**2 / (2 * sigma_v**2))
+#  As q is not yet defined we'll try with a unitary importance function
+q = lambda x, x1, y: 1
 pyx = lambda x, y: norm.pdf((y - h(x, 0)) / sigma_v)
 pxx = lambda x, x1, k: norm.pdf((x - f(x1, k, 0)) / sigma_w)
-
-# State variables initialization
-X_es = np.zeros(N)
 
 # Crossover and mutation parametrers
 alpha = 0.4  #
@@ -72,31 +74,68 @@ for l in range(L):
         PYX = np.zeros(N)
         PXX = np.zeros(N)
 
+        # Initialize Variables vector
+        X = np.zeros(N)
         for i in range(N):
             # Sampling
-            X_k[i] = np.random.normal(loc = f(X_k1[i], l, 0), scale=sigma_w)
+            X[i] = np.random.normal(loc=f(X_k1[i], l, 0), scale=sigma_w)
 
             # Calculate probabilities and importance function
-            Q[i] = q(X_k[i], Y_measured[l])
-            PYX[i] = pyx(Y_measured[l], X_k[i])
-            PYX[i] = pxx(X_k[i], X_k1[i])
+            PYX[i] = pyx(Y_measured[l], X[i])
+            PXX[i] = pxx(X[i], X_k1[i], l)
+            Q[i] = q(X[i], X_k1[i], Y_measured[l])  # Still focus on a deeper understanding
 
         # Compute weights
-        Ws = W_k1 * PXX * PYX / Q
-        W = Ws / sum(Ws)
+        Ws = W_k1 * PXX * PYX / Q  # TODO Compute Weights with a function
+        W = Ws / sum(Ws)  # Normalize weights to one
 
         # State variables estimation
-        X_es[l] = sum(W * X_k)
+        X_estimated[l] = sum(W * X)
 
         # Perform mutation and crossover operators
         # Crossover
-        CL, CH = divide(X_k, W)
+        CL, CH = divide(X, W)
         rL = np.random.uniform(low=0, high=1, size=len(CL))
         for x in range(len(CL)):
             arg_H = np.random.choice(CH)
-            X_k[CL[x]] = X_k[CL[x]] * alpha + (1-alpha) * X_k[arg_H]
-            if rL[x] <= p_m:
-                X_k[CL[x]] = 2 * X_k[arg_H] - X_k[CL[x]]
+            X[CL[x]] = X[CL[x]] * alpha + (1-alpha) * X[arg_H]
+            if rL[x] <= p_m:  # Mutation
+                X[CL[x]] = 2 * X[arg_H] - X[CL[x]]
 
+        # Recalculate weights
+        Q = np.zeros(N)
+        PYX = np.zeros(N)
+        PXX = np.zeros(N)
 
+        for i in range(N):
+            # Calculate probabilities and importance function
+            PYX[i] = pyx(Y_measured[l], X[i])
+            PXX[i] = pxx(X[i], X_k1[i], l)
+            Q[i] = q(X[i], X_k1[i], Y_measured[l])  # Still focus on a deeper understanding
 
+        # Compute weights
+        Ws = W_k1 * PXX * PYX / Q  # TODO Compute Weights with a function
+        W = Ws / sum(Ws)  # Normalize weights to one
+
+        # Resampling
+        U = np.sort(abs(np.random.uniform(-1, 0, N)))  # Generate N elements fo Uniform distribution
+        sum_w = 0  # Accumulated weights
+        i = 0  # weight index
+
+        # Initialize sample values and previous weights associated
+        W_k = np.zeros(N)
+        X_k = np.zeros(N)
+
+        # Multinomial resampling implementation
+        for j in range(N):
+
+            while not sum_w < U[j] <= sum_w + W[i]:
+                sum_w += W[i]
+                i += 1
+
+            W_k[j] = W[i]
+            X_k[j] = X[i]
+
+plt.plot(X_true[:200])
+plt.plot(X_estimated[:200])
+plt.show()
